e15b05d53a77b9f5d8fc7277b03217ca6bf699c7
ktodoran <ktodoran@gmail.com>
2021-09-23 16:28:51 -0400

    Updated Pages files

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
-import Cart from "../components/Cart";
 
 const Home = () => {
   return (
     <div className="container">
       <CategoryMenu />
       <ProductList />
-      <Cart />
     </div>
   );
 };
