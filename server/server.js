0e027d864db80051538cb818324d2d1b8b22c79d
ktodoran <ktodoran@gmail.com>
2021-09-24 09:31:00 -0400

    Final Touches

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
@@ -19,7 +18,8 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/images', express.static(path.join(__dirname, '../client/images')));
+// Serve up static assets
+app.use('/images', express.static(path.join(__dirname, '../client/public/images')));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
